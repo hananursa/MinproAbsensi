5c8a966e7e888f34d238200342c6f7df37d9f853
hananursa <salsabillahhana@gmail.com>
2020-06-02 08:40:46 +0700

    penggantian url

--- app/src/main/java/id/ac/polinema/absensiguruprivate/rest/ApiClient.java
+++ app/src/main/java/id/ac/polinema/absensiguruprivate/rest/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "https://hananursa.000webhostapp.com/index.php/api/";
+    public static final String BASE_URL = "https://hanasabil.000webhostapp.com/index.php/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
